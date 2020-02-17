f34d8cb983ba6115598673079842ad5c48910b87
Huberkrok <59973338+Huberkrok@users.noreply.github.com>
2020-02-17 15:03:48 +0100

    display_stats, max_hp , display_inv

--- ui.py
+++ ui.py
@@ -1,10 +1,8 @@
 def display_board(board):
     '''
     Displays complete game board on the screen
-
-
     Returns:
-    Nothing 
+    Nothing
     '''
     text = ""
     for i in range(len(board)):
@@ -13,3 +11,29 @@ def display_board(board):
         text += "\n"
     print(text)
 
+
+inv = {"HP Potion", "Mana Potion", "Beginner Sword"}
+
+
+def display_inv():
+    print(inv)
+    listed_items = 0
+    for i in inv:
+        listed_items += 1
+        if listed_items >= 10:
+            print("you can't carry more items")
+
+
+HP = 5
+MP = 10
+
+
+def max_hp():
+    max_health_points = 10
+    if HP >= max_health_points:
+        HP = max_health_points
+    return HP
+
+
+def display_stats():
+    print("HP =", HP, "MP =", MP)
